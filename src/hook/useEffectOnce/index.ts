9c043d38db47a5be18bce3afd3a6030299cb0173
Jan Barášek <janbarasek@gmail.com>
2023-02-26 18:37:29 +0100

    Add useEffectOnce.

---
+++ src/hook/useEffectOnce/index.ts
@@ -0,0 +1,26 @@
+import { useEffect, useRef, useState } from 'react';
+
+export const useEffectOnce = (effect: () => void | (() => void)) => {
+  const destroyFunc = useRef<void | (() => void)>();
+  const effectCalled = useRef(false);
+  const renderAfterCalled = useRef(false);
+  const [val, setVal] = useState<number>(0);
+
+  if (effectCalled.current) {
+    renderAfterCalled.current = true;
+  }
+
+  useEffect(() => {
+    if (!effectCalled.current) {
+      destroyFunc.current = effect();
+      effectCalled.current = true;
+    }
+
+    setVal((val) => val + 1);
+
+    return () => {
+      if (!renderAfterCalled.current) return;
+      if (destroyFunc.current) destroyFunc.current();
+    };
+  }, []);
+};
